f7b80eea56cf0af4889820e9e60bd44c739aae45
choco-tyranno <choco.tyranno@gmail.com>
2021-10-03 23:50:51 +0900

    feat: add base layout.

--- StudyHilt/app/src/main/java/com/choco_tyranno/studyhilt/ui/main/MainFragment.kt
+++ StudyHilt/app/src/main/java/com/choco_tyranno/studyhilt/ui/main/MainFragment.kt
@@ -1,10 +1,28 @@
 package com.choco_tyranno.studyhilt.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.choco_tyranno.studyhilt.R
-class MainFragment : Fragment() {
+import com.choco_tyranno.studyhilt.ui.second.SecondActivity
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_activity).setOnClickListener {
+            val intent = Intent(requireContext(), SecondActivity::class.java)
+            startActivity(intent)
+        }
+        view.findViewById<Button>(R.id.button_fragment).setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_secondFragment)
+        }
+
+
+    }
 }
--- StudyHilt/app/src/main/java/com/choco_tyranno/studyhilt/ui/main/SecondFragment.kt
+++ StudyHilt/app/src/main/java/com/choco_tyranno/studyhilt/ui/main/SecondFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.choco_tyranno.studyhilt.R
-class SecondFragment : Fragment() {
+class SecondFragment : Fragment(R.layout.fragment_second) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button_back).setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_mainFragment)
+        }
+    }
 }
